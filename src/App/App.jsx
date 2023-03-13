e4e903b1c7b97f8c8c51141e2d289976410fd98b
TanThanh-CityBoi <tanthanhe@gmail.com>
2023-03-13 12:07:57 +0700

    init source

---
+++ src/App/App.jsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+import NormalizeStyles from './NormalizeStyles';
+import BaseStyles from './BaseStyles';
+// import Toast from './Toast';
+import Routes from './Routes';
+import './fontStyles.css';
+
+const App = () => (
+  <Fragment>
+    <NormalizeStyles />
+    <BaseStyles />
+    {/* <Toast /> */}
+    <Routes />
+  </Fragment>
+);
+
+export default App;
---
+++ src/App/Routes.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Router, Route, Redirect } from 'react-router-dom';
+
+// import Project from 'Project';
+// import Authenticate from 'Auth/Authenticate';
+// import PageError from 'shared/components/PageError';
+
+const Routes = () => (
+  <Router>
+      {/* <Redirect exact from="/" to="/project" /> */}
+      {/* <Route path="/authenticate" component={Authenticate} /> */}
+      {/* <Route path="/project" component={Project} /> */}
+      {/* <Route component={PageError} /> */}
+  </Router>
+);
+
+export default Routes;
