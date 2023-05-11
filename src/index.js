3a2743058a0bb9a1af23400e01d54cf1431b33a5
thanh.nguyen <thanh.nguyen@codestringers.com>
2023-05-11 12:23:59 +0700

    yarn lock

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import store from "./redux/configStore";
-import { Provider } from "react-redux";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import { config } from "./config";
-import "antd/dist/antd.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {Provider} from 'react-redux';
+import store from './redux/configStore';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <GoogleOAuthProvider clientId={config.GOOGLE_CLIENT_ID}>
-        <App />
-      </GoogleOAuthProvider>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
