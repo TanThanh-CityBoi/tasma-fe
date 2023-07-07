47718912392aa4624e85c0266df83a4910096a80
Bui Phuoc <buiphuoc@Phuocs-Mac.local>
2023-07-07 23:04:19 +0700

    fix error loading page pomodorotimer

--- src/pages/Project/PomodoroTimer/PomodoroTimer.jsx
+++ src/pages/Project/PomodoroTimer/PomodoroTimer.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Iframe from 'react-iframe'
 
 export default function PomodoroTimer() {
-  return (
-    <Iframe url="http://127.0.0.1:5500/simple_pomodoro/"
-        width="100%"
-        height="750px"
-        // display="block"
-        position="relative"
-        frameBorder={0}
+    const [error, setError] = useState(false);
 
-        />
-  )
+    const handleIframeError = () => {
+      setError(true);
+    };
+
+    return (
+      <div>
+        {error ? (
+          <div>Server error</div>
+        ) : (
+          <Iframe
+            src="http://127.0.0.1:5500/simple_pomodoro/"
+            width="100%"
+            height="750px"
+            frameBorder={0}
+            onError={handleIframeError}
+          />
+        )}
+      </div>
+    );
 }
