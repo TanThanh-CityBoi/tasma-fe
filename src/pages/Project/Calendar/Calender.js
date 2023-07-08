c7bfc7df5a935a4b2d76dd72456d1906c0782fa8
TanThanh-CityBoi <tanthanhe@gmail.com>
2023-07-08 10:11:26 +0700

    calendar

--- src/pages/Project/Calendar/Calender.js
+++ src/pages/Project/Calendar/Calender.js
@@ -1,93 +1,140 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useSWR from "swr";
 import FullCalendar from "@fullcalendar/react"; // must go before plugins
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
-import interactionPlugin, { Draggable } from "@fullcalendar/interaction";
-const defaultEvents = [
-  {
-    id: "aahahahhahah",
-    start: "2023-06-08T13:00",
-    end: "2023-06-18T13:45",
-    title: "Lunch @ Butcher's",
-    allDay: false,
-    color: "#009788",
-    // url: "url click ", // Có thể xài thằng này để navigate tới cái trang edit sự kiện ( nhưng cũng có thể open modal bằng thằng event onClick, mày tự implement nha.)
-  },
-  {
-    id: 2,
-    start: "2023-06-04T15:00",
-    end: "2023-06-24T16:00",
-    title: "General orientation",
-    allDay: false,
-    color: "#ff9900",
-  },
-  {
-    id: 3,
-    start: "2023-06-23T18:00",
-    end: "2023-06-29T22:00",
-    title: "Dexter BD",
-    allDay: false,
-    color: "#3f51b5",
-  },
-  {
-    id: 4,
-    start: "2023-06-24T10:30",
-    end: "2023-06-25T11:30",
-    title: "Stakeholder mtg.",
-    allDay: false,
-    color: "#f44437",
-  },
-];
+import interactionPlugin from "@fullcalendar/interaction";
+import { useSelector, useDispatch } from "react-redux";
+import { GET_ALL_PROJECTS_SAGA } from "../../../redux/constants/ProjectConst";
+// const defaultEvents = [
+//    {
+//       id: "aahahahhahah",
+//       start: "2023-06-08T13:00",
+//       end: "2023-06-18T13:45",
+//       title: "Lunch @ Butcher's",
+//       allDay: false,
+//       color: "#009788",
+//       // url: "url click ", // Có thể xài thằng này để navigate tới cái trang edit sự kiện ( nhưng cũng có thể open modal bằng thằng event onClick, mày tự implement nha.)
+//    },
+//    {
+//       id: 2,
+//       start: "2023-06-04T15:00",
+//       end: "2023-06-24T16:00",
+//       title: "General orientation",
+//       allDay: false,
+//       color: "#ff9900",
+//    },
+//    {
+//       id: 3,
+//       start: "2023-06-23T18:00",
+//       end: "2023-06-29T22:00",
+//       title: "Dexter BD",
+//       allDay: false,
+//       color: "#3f51b5",
+//    },
+//    {
+//       id: 4,
+//       start: "2023-06-24T10:30",
+//       end: "2023-06-25T11:30",
+//       title: "Stakeholder mtg.",
+//       allDay: false,
+//       color: "#f44437",
+//    },
+// ];
+
+const Colors = {
+   BACKLOG: "#c4c5c6",
+   INPROGRESS: "#62a0ce",
+   UNDERREVIEW: "#F9D900",
+   CANCELLED: "#E50000",
+   DONE: "#5fb03e",
+};
 
 const Calendar = () => {
-  const fetchCalendarEvents = () => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(defaultEvents);
-      }, 1000);
-    });
-  };
+   const dispatch = useDispatch();
+   const projects = useSelector((state) => state.ProjectReducer.projects) || [];
+   const [currentProject, setCurrentProject] = useState(projects[0] || {});
+   const defaultEvents = currentProject?.tasks?.map((task) => {
+      return {
+         id: task?.id,
+         start: task?.createdDate,
+         end: task?.dueDate,
+         title: task?.name,
+         allDay: true,
+         color: Colors[task?.status || "BACKLOG"],
+      };
+   });
 
-  const { data, isLoading, error } = useSWR(
-    "get_calendar_events",
-    fetchCalendarEvents
-  );
-  const handleEventDrop = (e) => {
-    alert(
-      JSON.stringify(
-        { ...e.event._instance.range, id: e.event._def.publicId },
-        null,
-        2
-      )
-    );
-  };
+   const fetchCalendarEvents = () => {
+      return new Promise((resolve) => {
+         setTimeout(async () => {
+            resolve(defaultEvents);
+         }, 500);
+      });
+   };
 
-  const handleEventClick = (e) => {
-    alert(e.event._def.publicId);
-  };
+   const { data, isLoading, error } = useSWR("get_calendar_events", fetchCalendarEvents);
+   const handleEventDrop = (e) => {
+      alert(JSON.stringify({ ...e.event._instance.range, id: e.event._def.publicId }, null, 2));
+   };
 
-  if (isLoading) return <h1>Loading...</h1>;
-  if (error) return <h3>Error: {JSON.stringify(error)}</h3>;
+   const handleEventClick = (e) => {
+      alert(e.event._def.publicId);
+   };
 
-  return (
-    <FullCalendar
-      events={data}
-      eventClick={handleEventClick}
-      initialView=""
-      plugins={[dayGridPlugin, interactionPlugin]}
-      eventContent={renderEventContent}
-      eventDrop={handleEventDrop}
-      editable={true}
-      droppable={true}
-    />
-  );
+   useEffect(() => {
+      dispatch({
+         type: GET_ALL_PROJECTS_SAGA,
+      });
+      return () => {};
+   }, []);
+
+   if (isLoading) return <h1>Loading...</h1>;
+   if (error) return <h3>Error: {JSON.stringify(error)}</h3>;
+   //
+   return (
+      <div>
+         <div className="d-flex justify-content-end mb-4">
+            <select
+               name="projects"
+               id="projects"
+               onChange={(e) => {
+                  setCurrentProject(projects[e.target.value]);
+               }}
+               className="p-2"
+               style={{ minWidth: "200px" }}
+            >
+               {projects?.map((pr, _id) => {
+                  return <option value={_id}>{pr.name}</option>;
+               })}
+            </select>
+         </div>
+         <FullCalendar
+            headerToolbar={{
+               left: "prev,next today",
+               center: "title",
+               right: "dayGridMonth,dayGridWeek,dayGridYear",
+            }}
+            events={defaultEvents}
+            eventClick={handleEventClick}
+            initialView="dayGridMonth"
+            plugins={[dayGridPlugin, interactionPlugin]}
+            eventContent={renderEventContent}
+            eventDrop={handleEventDrop}
+            editable={true}
+            droppable={true}
+            initialEvents={defaultEvents}
+         />
+      </div>
+   );
 };
+
 function renderEventContent(eventInfo) {
-  return (
-    <div>
-      <b>{eventInfo.timeText} </b>
-      <i>{eventInfo.event.title}</i>
-    </div>
-  );
+   return (
+      <div>
+         <b>{eventInfo.timeText} </b>
+         <i>{eventInfo.event.title}</i>
+      </div>
+   );
 }
+
 export default Calendar;
